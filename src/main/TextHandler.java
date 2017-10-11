6c04caaa4163947d7d92a3bdea80bb6cdb93fd33
squeakadoodle <squeakadoodle6084@gmail.com>
2017-10-10 22:07:47 -0700

    Added offline testing support

---
+++ src/main/TextHandler.java
@@ -0,0 +1,38 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import tokens.Response;
+import utilities.Handler;
+
+public class TextHandler {
+	
+	public TextHandler() {};
+	
+	public void process(String message) {
+		ArrayList<Response> responses = new ArrayList<Response>();
+		// Checks if a message begins with the bot command prefix
+		if (message.startsWith(Constants.PREFIX)) { // if invoked
+			for( Handler h : Brain.invokers ) { // try each invocation handler
+				String text = h.process(message); // process individual invocation handler
+				if( !text.equals("") ) { // if this produces a result
+					responses.add(new Response(text, h.getPriority())); // add it to the list of potential responses
+				}
+			}
+		} else { // if not being invoked
+			for( Handler h : Brain.responders ) { // then try each auto handler
+				String text = h.process(message); // process individual handler
+				if( !text.equals("") ) { // if this produces a result
+					responses.add(new Response(text, h.getPriority())); // add it to the list of potential responses
+				}
+			}
+		}
+		if( responses.size() != 0 ) { // if any response exists
+			Response[] options = new Response[responses.size()]; // create a static array of response options
+			Arrays.sort(options); // sort these options
+			System.out.println(options[0]);
+		}
+	}
+	
+}
