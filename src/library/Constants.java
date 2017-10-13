03db7d2f6879c80ddc29a9f0a3efe719d6e37b31
InfinityPhase <4tazracer@gmail.com>
2017-10-12 22:49:46 -0700

    Moved files to library package

--- src/library/Constants.java
+++ src/library/Constants.java
@@ -1,4 +1,4 @@
-package main;
+package library;
 
 public class Constants {
 	public static final String PREFIX = "==> ";
--- src/library/Library.java
+++ src/library/Library.java
@@ -1,4 +1,4 @@
-package main;
+package library;
 
 public class Library {
 	// stores appropriate reactions for various things
