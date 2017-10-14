1210a3584fa8185ff9f8293462349b5214c51ff4
squeakadoodle <squeakadoodle6084@gmail.com>
2017-10-14 13:41:42 -0700

    Bug Fixes

--- src/invokers/VoteInvoker.java
+++ src/invokers/VoteInvoker.java
@@ -41,8 +41,8 @@ public class VoteInvoker extends Handler {
 				if( !Brain.guildIndex.get(event.getGuild()).polls.keySet().contains(name) ) {
 					return build( "Poll \"" + name + "\" does not exist.");
 				}
-				Brain.guildIndex.get(event.getGuild()).polls.remove(name);
 				return build(Brain.guildIndex.get(event.getGuild()).polls.get(name).end());
+				Brain.guildIndex.get(event.getGuild()).polls.remove(name);
 			} else if( action.equals("cast") ) {
 				if( tokens.size() < 4 ) {
 					return build( "Syntax Error: Insufficient parameters.");
