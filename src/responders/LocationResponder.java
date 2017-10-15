8cffcedf38321cc67ce76bdfac1e4478ea8cd762
squeakadoodle <squeakadoodle6084@gmail.com>
2017-10-14 17:59:56 -0700

    Fixed a major bug

--- src/responders/LocationResponder.java
+++ src/responders/LocationResponder.java
@@ -21,33 +21,33 @@ public class LocationResponder extends Handler {
 		
 		if( tokens.contains("where's") || tokens.contains("where") ) {
 			if( Constants.DEBUG ) {System.out.println("\t\t\t\tLocation query detected.");}
-			for( String person : Brain.guildIndex.get(event.getChannel()).people.keySet() ) {
+			for( String person : Brain.guildIndex.get(event.getGuild()).people.keySet() ) {
 				if( messageText.contains(person) ) {
 					if( Constants.DEBUG ) {System.out.println("\t\t\t\t\tName \"" + person + "\" detected.");}
 					if( Constants.DEBUG ) {System.out.println("\t\t\t\t\tLocationResponder triggered.");}
-					response = person + " is at " + Brain.guildIndex.get(event.getChannel()).people.get(person) + ".";
+					response = person + " is at " + Brain.guildIndex.get(event.getGuild()).people.get(person) + ".";
 					break;
 				}
 			}
 			for( String token : tokens ) {
-				if( Brain.guildIndex.get(event.getChannel()).people.keySet().contains(token) ) {
+				if( Brain.guildIndex.get(event.getGuild()).people.keySet().contains(token) ) {
 					if( Constants.DEBUG ) {System.out.println("\t\t\t\t\tLocation \"" + token + "\" detected.");}
 					if( Constants.DEBUG ) {System.out.println("\t\t\t\t\tLocationResponder triggered.");}
-					response = token + " is at " + Brain.guildIndex.get(event.getChannel()).people.get(token) + ".";
+					response = token + " is at " + Brain.guildIndex.get(event.getGuild()).people.get(token) + ".";
 					break;
 				}
 			}
 		} else if( tokens.contains("who") && tokens.contains("is") && tokens.contains("at") || tokens.contains("who's") && tokens.contains("at") ) {
 			if( Constants.DEBUG ) {System.out.println("\t\t\t\tPerson query detected.");}
-			for( String location : Brain.guildIndex.get(event.getChannel()).locations.keySet() ) {
+			for( String location : Brain.guildIndex.get(event.getGuild()).locations.keySet() ) {
 				if( messageText.contains(location) ) {
 					if( Constants.DEBUG ) {System.out.println("\t\t\t\t\tLocation \"" + location + "\" detected.");}
-					if( !Brain.guildIndex.get(event.getChannel()).locations.containsKey(location) ) {
+					if( !Brain.guildIndex.get(event.getGuild()).locations.containsKey(location) ) {
 						if( Constants.DEBUG ) {System.out.println("\t\t\t\t\tLocation not set.");}
 						if( Constants.DEBUG ) {System.out.println("\t\t\t\t\tLocationResponder triggered.");}
 						return build(location + " is not a set location.");
 					}
-					ArrayList<String> locals = Brain.guildIndex.get(event.getChannel()).locations.get(location);
+					ArrayList<String> locals = Brain.guildIndex.get(event.getGuild()).locations.get(location);
 					if( locals.size() == 0 ) {
 						if( Constants.DEBUG ) {System.out.println("\t\t\t\t\tLocation \t" + location + "\" empty.");}
 						if( Constants.DEBUG ) {System.out.println("\t\t\t\t\tLocationResponder triggered.");}
