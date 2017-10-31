2b44a13c0405af365c2db475f55ac41d65509686
InfinityPhase <4tazracer@gmail.com>
2017-10-31 16:39:34 -0700

    Using console to get password with masking

--- src/library/Variables.java
+++ src/library/Variables.java
@@ -2,5 +2,5 @@ package library;
 
 public class Variables {
 	// Dynamic global variables
-
+	public static char[] password;
 }
--- src/main/Brain.java
+++ src/main/Brain.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.io.Console;
 
 import commands.CommandHandler;
 import invokers.EchoInvoker;
@@ -12,6 +13,7 @@ import invokers.NicknameInvoker;
 import invokers.VoteInvoker;
 import invokers._8BallInvoker;
 import library.Constants;
+import library.Variables;
 import responders.LocationResponder;
 import responders.MentionResponder;
 import responders.NicknameResponder;
@@ -27,10 +29,12 @@ public class Brain {
 	 * 	- Responders ignore case by setting messages to lower case before parsing
 	 *  - Constants cannot be altered; Use for... Constants
 	 *  - Variables should be mutable; Use for changing values
+	 *  - When a module is altered, update the ID with Eclipse
 	 */
-	
+
 	public static TokenParser tp = new TokenParser();
 	public static SimpleDateFormat sdf = new SimpleDateFormat( Constants.DATEFORMAT );
+	public static Console console = System.console();
 
 	public static ArrayList<Handler> invokers = new ArrayList<Handler>();
 	public static ArrayList<Handler> responders = new ArrayList<Handler>();
@@ -41,15 +45,15 @@ public class Brain {
 	public static VoteInvoker voteInvoker = new VoteInvoker();
 	public static _8BallInvoker _8ballInvoker = new _8BallInvoker();
 	public static NicknameInvoker nicknameInvoker = new NicknameInvoker();
-	
+
 	/* Auto Handlers */
 	public static MentionResponder mentionResponder = new MentionResponder();
 	public static LocationResponder locationResponder = new LocationResponder();
 	public static NicknameResponder nicknameResponder = new NicknameResponder();
-	
+
 	/* Gigantic Variable Library */
 	public static HashMap<IGuild, GuildInfo> guildIndex = new HashMap<IGuild, GuildInfo>();
-	
+
 	public static void main(String[] args) {
 
 		init();
@@ -59,25 +63,29 @@ public class Brain {
 			System.out.println("# java -jar SimpleResponder.jar TOKEN");
 			System.exit(0);
 		}
-		
-		if( args.length >= 3 ) {
-			// Get the encryption password
-			
+
+		// Get the encryption password
+		if( Constants.SAVESTATE ) {
+			Variables.password = console.readPassword();
+
+			if( Variables.password.length <= 0 ) {
+				System.out.println("WARNING: Password is empty");
+			}
 		}
-		
+
 		// Gets token from arguments
 		String token = args[0];
 
 		IDiscordClient cli = BotUtils.getBuiltDiscordClient(token);
 		if( Constants.DEBUG ) {System.out.println("Client built successfully.");}
-		
+
 		cli.getDispatcher().registerListener(new CommandHandler());
 		if( Constants.DEBUG ) {System.out.println("Listener established successfully.");}
-		
+
 		// Only login after all event registering is done
 		cli.login();
 		if( Constants.DEBUG ) {System.out.println("Client logged in.");}
-		
+
 		long startTime = System.currentTimeMillis();
 		while( true ) {
 			// I'm sure that this won't cause a slowdown...
@@ -85,7 +93,7 @@ public class Brain {
 			if( ( ( System.currentTimeMillis() - startTime ) >= Constants.SAVETIME ) && Constants.SAVESTATE ) {
 				// Check to save state of CARIS
 				if( Constants.DEBUG ) { System.out.println("Saving CARIS State..."); }
-				
+
 				String fileName = ( Constants.PREPENDDATE ? sdf.format( Calendar.getInstance().getTime() ) : "" ) + Constants.SAVEFILE + Constants.SAVEEXTENTION;
 				// Open JSON file
 				// Clear contents if exists
@@ -93,8 +101,8 @@ public class Brain {
 				// Save stream to JSON file
 				// Read file:
 				// try( BufferedReader br = new BufferedReader( new InputStreamReader( new FileInputStream( new File( fileName ) ), Constants.ENCODING ) ); ) {} catch( EXCEPTION e ) {}
-				
-				
+
+
 				startTime = System.currentTimeMillis();
 			}
 		}
