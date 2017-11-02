8e298a43b0fbaeea7fe7083a85febb74e40c2b91
InfinityPhase <4tazracer@gmail.com>
2017-11-02 15:14:30 -0700

    In progress

--- src/library/Constants.java
+++ src/library/Constants.java
@@ -5,4 +5,9 @@ public class Constants {
 	public static final String NAME = "CARIS";
 	public static final boolean OFFLINE = false;
 	public static final boolean DEBUG = true;
+	
+	public static final String DEBUG_LOG = "DebugLog";
+	public static final String FILE_EXTENTION = ".caris";
+	public static 
+	
 }
---
+++ src/utilities/Logger.java
@@ -0,0 +1,32 @@
+package utilities;
+
+import library.Constants;
+
+public class Logger {
+
+	public void toConsole(String message) {
+		// Equivalent to System.out.println();
+		System.out.println(message);
+		
+	}
+	
+	public void toLog(String message) {
+		Constants.
+	}
+	
+	public void status() {
+		
+	}
+	
+	public void debugConsole() {
+		
+	}
+	
+	public void debugLog() {
+		
+	}
+	
+	public void debugOut() {
+		
+	}
+}
