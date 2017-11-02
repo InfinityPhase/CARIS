27077c1d7c61406e761c8fcfbd454fd89645a40f
InfinityPhase <4tazracer@gmail.com>
2017-11-02 15:15:35 -0700

    Implemented changes to Constants file

--- src/library/Constants.java
+++ src/library/Constants.java
@@ -1,8 +1,19 @@
 package library;
 
 public class Constants {
-	public static final String PREFIX = "==> ";
-	public static final String NAME = "CARIS";
-	public static final boolean OFFLINE = false;
-	public static final boolean DEBUG = true;
+        public static final String PREFIX = "==> ";
+        public static final String NAME = "CARIS";
+        public static final boolean OFFLINE = false;
+        public static final boolean DEBUG = true;
+        
+        /* Settings for saving the state of CARIS */
+        // SAVETIME uses milliseconds
+        // Default is 6000000 ms
+        public static final int SAVETIME = 20000;
+        public static final boolean SAVESTATE = true;
+        public static final boolean PREPENDDATE = true;
+        public static final String SAVEFILE = "CARIS_State";
+        public static final String SAVEEXTENTION = ".caris";
+        public static final String DATEFORMAT = "yyyyMMddhhmm";
+        public static final String ENCODING = "UTF-8";
 }
