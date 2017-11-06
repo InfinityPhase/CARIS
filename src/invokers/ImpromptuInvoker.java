a39afa34d41fae9fe779ef98a84146d578094398
InfinityPhase <4tazracer@gmail.com>
2017-11-06 14:16:32 -0800

    Swapped to using lists for topic storage

--- src/invokers/ImpromptuInvoker.java
+++ src/invokers/ImpromptuInvoker.java
@@ -24,10 +24,10 @@ public class ImpromptuInvoker extends Invoker {
 			Brain.log.debugOut( "Impromptu topic generator invoked" );
 
 			if( containsIgnoreCase( keywords[1] , tokens.get( 1 ) ) ) {
-				// Get random thing from Concrete
+				// Random Concrete
 				Brain.log.debugOut( "Choosing random concrete topic..." );
-				if( ImpromptuTopics.Concrete.length != 0 ) {
-				response = ImpromptuTopics.Concrete[ rand.nextInt( ImpromptuTopics.Concrete.length ) ];
+				if( ImpromptuTopics.Concrete.size() != 0 ) {
+					response = ImpromptuTopics.Concrete.get( rand.nextInt( ImpromptuTopics.Concrete.size() ) );
 				} else {
 					Brain.log.debugOut("Failed to find concrete topics");
 					response = "What, you think I should tell you what is real?";
@@ -35,9 +35,8 @@ public class ImpromptuInvoker extends Invoker {
 			} else if( containsIgnoreCase( keywords[2], tokens.get( 1 ) ) ) {
 				// Random Abstract
 				Brain.log.debugOut( "Choosing random abstract topic..." );
-				if( ImpromptuTopics.Abstract.length != 0 ) {
-					
-				response = ImpromptuTopics.Abstract[ rand.nextInt( ImpromptuTopics.Abstract.length ) ];
+				if( ImpromptuTopics.Abstract.size() != 0 ) {
+					response = ImpromptuTopics.Abstract.get( rand.nextInt( ImpromptuTopics.Abstract.size() ) );
 				} else {
 					Brain.log.debugOut("Failed to find abstract topics");
 					response = "As if I have time to deal with fake information.";
@@ -45,8 +44,8 @@ public class ImpromptuInvoker extends Invoker {
 			} else if( containsIgnoreCase( keywords[3], tokens.get( 1 ) ) ) {
 				// Random Quote
 				Brain.log.debugOut( "Choosing random quote..." );
-				if( ImpromptuTopics.Quote.length != 0 ) {
-				response = ImpromptuTopics.Quote[ rand.nextInt( ImpromptuTopics.Quote.length ) ];
+				if( ImpromptuTopics.Quote.size() != 0 ) {
+					response = ImpromptuTopics.Quote.get( rand.nextInt( ImpromptuTopics.Quote.size() ) );
 				} else {
 					Brain.log.debugOut("Failed to find quote topics");
 					response = "How should I know what you strange beings say?";
@@ -54,8 +53,8 @@ public class ImpromptuInvoker extends Invoker {
 			} else if( containsIgnoreCase( keywords[4], tokens.get( 1 ) ) ) {
 				// Random Current
 				Brain.log.debugOut( "Choosing random current event..." );
-				if( ImpromptuTopics.Current.length != 0 ) {
-					response = ImpromptuTopics.Current[ rand.nextInt( ImpromptuTopics.Current.length ) ];
+				if( ImpromptuTopics.Current.size() != 0 ) {
+					response = ImpromptuTopics.Current.get( rand.nextInt( ImpromptuTopics.Current.size() ) );
 				} else {
 					Brain.log.debugOut("Failed to find current event topics");
 					response = "You think I keep track of world events? Try again.";
