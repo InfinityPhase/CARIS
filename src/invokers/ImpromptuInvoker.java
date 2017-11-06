00fe7368b460a881c62638d23019e5e2778b2ba7
InfinityPhase <4tazracer@gmail.com>
2017-11-06 09:56:33 -0800

    Added new Variables

---
+++ src/invokers/ImpromptuInvoker.java
@@ -0,0 +1,43 @@
+package invokers;
+
+import main.Brain;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import tokens.Response;
+
+public class ImpromptuInvoker extends Invoker {
+	
+	String[][] keywords = { { "imp", "impromptu" },
+			{ "concrete", "physical", "real" },
+			{ "abstract", "idea", "ideas" },
+			{ "quote", "quotes", "phrase", "phrases", "saying", "sayings" },
+			{ "current", "news", "event", "events", "stories" } };
+	
+	@Override
+	public Response process( MessageReceivedEvent event ) {
+		setup( event );
+		
+		if( containsIgnoreCase( keywords[0], tokens.get( 0 ) ) ) {
+			Brain.log.debugOut( "Impromptu topic generator invoked" );
+			
+			if( containsIgnoreCase( keywords[1] , tokens.get( 1 ) ) ) {
+				// Get random thing from impConcrete
+				Brain.log.debugOut( "Choosing random concrete topic..." );
+			} else if( containsIgnoreCase( keywords[2], tokens.get( 1 ) ) ) {
+				// Random impAbstract
+				Brain.log.debugOut( "Choosing random abstract topic..." );
+			} else if( containsIgnoreCase( keywords[3], tokens.get( 1 ) ) ) {
+				// Random impQuote
+				Brain.log.debugOut( "Choosing random quote..." );
+			} else if( containsIgnoreCase( keywords[4], tokens.get( 1 ) ) ) {
+				// Random impCurrent
+				Brain.log.debugOut( "Choosing random current event..." );
+				response = "You think I keep track of world events? Try again.";
+			} else {
+				Brain.log.debugOut( "No impromptu event generated" );
+				response = "I can't understand your gibberish. Try again.";
+			}
+		}
+		
+		return build();
+	}
+}
--- src/library/Variables.java
+++ src/library/Variables.java
@@ -2,4 +2,10 @@ package library;
 
 public class Variables {
 	// Dynamic global variables
+	
+	// Impromptu wordlist
+	public static String[] impConcrete = {};
+	public static String[] impAbstract = {};
+	public static String[] impQuote = {};
+	public static String[] impCurrent = {};
 }
