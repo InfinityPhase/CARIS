fff8314a4f34222ee44dd95f2d97f385188ea4f7
squeakadoodle <squeakadoodle6084@gmail.com>
2017-11-15 22:18:21 -0800

    Minor changes.

--- src/embedbuilders/ModuleStatusBuilder.java
+++ src/embedbuilders/ModuleStatusBuilder.java
@@ -11,8 +11,8 @@ public class ModuleStatusBuilder {
 	
 	public EmbedBuilder list(String name, HashMap<String, Boolean> modules) {
 		EmbedBuilder builder = new EmbedBuilder();
-		builder.withAuthorName("Module Status:");
-		builder.withTitle("**__" + name + "__**");
+		builder.withAuthorName("**__" + name + "__**");
+		builder.withDescription("*Module Status:*");
 		for( String s : modules.keySet() ) {
 			boolean state = modules.get(s);
 			if( state ) {
