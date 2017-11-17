db4ab89c41ec226424a51eb3db840856eb46221d
InfinityPhase <4tazracer@gmail.com>
2017-11-17 15:40:49 -0800

    Theoretically will work, probably will not

--- src/utilities/GuildLoader.java
+++ src/utilities/GuildLoader.java
@@ -4,6 +4,8 @@ import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import org.json.JSONObject;
+
 import com.vdurmont.emoji.Emoji;
 
 import main.Brain;
@@ -19,34 +21,15 @@ import sx.blah.discord.util.cache.Cache;
 public class GuildLoader {
 
 	// From the discord api website
-	long id;
-	String name;
-	String icon	;
-	String splash;
-	long owner_id;	
-	String region;	
-	long afk_channel_id;
-	int afk_timeout;
-	boolean embed_enabled;
-	long embed_channel_id;	
-	int verification_level;	
-	int default_message_notifications;	
-	int explicit_content_filter;
-	Cache<IRole> roles;
 	Emoji[] emojis;	
 	String[] features;	
 	int mfa_level;	
 	int application_id;
 	boolean widget_enabled;
 	long widget_channel_id;
-	joined_at;
 	boolean large;
 	boolean unavailable;
 	int member_count;
-	voice_states;
-	members;
-	Cache<IChannel> channels;
-	presences;
 
 	// WTF?
 	IShard shard;
@@ -58,32 +41,48 @@ public class GuildLoader {
 	private String endpoint = "https://discordapp.com/api/guilds/";
 	private URL url;
 	private HttpsURLConnection connection;
-	
+	private JSONObject rawGuild;
+
 	public GuildLoader( String guildID ) {
 		// FORMAT:
-		// curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "User-Agent: DiscordBot (infinityphase.com, 1)" -H "Authorization: Bot TOKENHERE" -X GET 'https://discordapp.com/api/guilds/{guildID}'
-		try {
-			url = new URL( endpoint );
-			connection = (HttpsURLConnection) url.openConnection();
-			
-			// Set params properly
-			connection.setRequestMethod( "GET" );
-			connection.setDoInput(true);
-			connection.setDoOutput(true);
-			
-			// Set the headers
-			connection.setRequestProperty( "Content-Type", "application/json" );
-			connection.setRequestProperty( "User-Agent", "DiscordBot (infinityphase.com, 1)" );
-			connection.setRequestProperty( "Authorization", "Bot " + Brain.token );
-		}
+		// curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "User-Agent: DiscordBot (example.com, 1)" -H "Authorization: Bot TOKENHERE" -X GET 'https://discordapp.com/api/guilds/{guildID}'
+
+		url = new URL( endpoint + guildID );
+		connection = (HttpsURLConnection) url.openConnection();
+
+		// Set params properly
+		connection.setRequestMethod( "GET" );
+		connection.setDoInput(true);
+		connection.setDoOutput(true);
+
+		// Set the headers
+		connection.setRequestProperty( "Content-Type", "application/json" );
+		connection.setRequestProperty( "User-Agent", "DiscordBot (infinityphase.com, 1)" );
+		connection.setRequestProperty( "Authorization", "Bot " + Brain.token );
+
+		rawGuild = new JSONObject( connection.getResponseMessage() );
+
 	}
 
 	public GuildLoader( Long guildID ) {
+		url = new URL( endpoint + guildID );
+		connection = (HttpsURLConnection) url.openConnection();
 
+		// Set params properly
+		connection.setRequestMethod( "GET" );
+		connection.setDoInput(true);
+		connection.setDoOutput(true);
+
+		// Set the headers
+		connection.setRequestProperty( "Content-Type", "application/json" );
+		connection.setRequestProperty( "User-Agent", "DiscordBot (infinityphase.com, 1)" );
+		connection.setRequestProperty( "Authorization", "Bot " + Brain.token );
+
+		rawGuild = new JSONObject( connection.getResponseMessage() );
 	}
 
 	public Guild buildGuild() {
-		return new Guild(shard, name, id, icon, owner_id, afk_channel_id, afk_timeout, region, verification_level, roles, channels, voiceChannels, users, joinTimes, categories);
+		return new Guild(shard, rawGuild.getString("name"), rawGuild.getLong("id"), rawGuild.getString("icon"), rawGuild.getLong("owner_id"), rawGuild.getLong("afk_channel_id"), rawGuild.getInt("afk_timeout"), rawGuild.getString("region"), rawGuild.getInt("verification_level"), (Cache<IRole>) rawGuild.get("roles"), (Cache<IChannel>) rawGuild.get("channels"), (Cache<IVoiceChannel>) rawGuild.get("voiceChannels"), (Cache<IUser>) rawGuild.get("users"), (Cache<Guild.TimeStampHolder>) rawGuild.get("joinTimes"), (Cache<ICategory>) rawGuild.get("categories") );
 	}
 
 }
