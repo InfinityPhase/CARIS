0de5429820fdbd78bba9f7055ab3764c9604474b
squeakadoodle <squeakadoodle6084@gmail.com>
2017-11-21 11:09:14 -0800

    Cleaned up extraneous commenting

--- src/commands/CommandHandler.java
+++ src/commands/CommandHandler.java
@@ -17,27 +17,6 @@ import utilities.BotUtils;
 
 public class CommandHandler {
 
-	// Deals with events
-
-	// You know what? Ignore this crap. I don't care right now.
-	// Go to https://github.com/decyg/d4jexamplebot/blob/master/src/main/java/com/github/decyg/CommandHandler.java
-	// When you do.
-	/*
-	// Maps command strings to the command implementations
-	// So, a command becomes functional
-	private static Map<String, Command> commandMap = new HashMap<>();
-
-	// Hum. We now deviate from the tutorial, because I do my own thing
-	// Also, I don't want to add more dependencies. Eh.
-
-	// Populate commandMap with functionality
-	// From tutorial:
-	// "Might be better practise to do this from an instantiated objects constructor"
-	static {
-		// TODO: STUFF HERE
-	}
-	 */
-
 	@EventSubscriber
 	public void onMessageRecieved(MessageReceivedEvent event) {
 		Brain.log.debugOut("Message received: \"" + event.getMessage().getContent() + "\" from User \"" + event.getAuthor().getName() + "\" on Guild \"" + event.getGuild().getName() + "\".", 1);
