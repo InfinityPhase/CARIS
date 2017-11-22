c73e5d8ecd670a5fb3764248b6e57d9f4126fc5a
InfinityPhase <4tazracer@gmail.com>
2017-11-21 21:28:35 -0800

    Forgot to rename functions in SuperEvent

--- src/commands/SuperEvent.java
+++ src/commands/SuperEvent.java
@@ -22,114 +22,114 @@ public class SuperEvent {
 	// A catch all class for events.
 	
     // Guild Events
-	@EventSubscriber public void name( AllUsersReceivedEvent event ) {}
-	@EventSubscriber public void name( GuildCreateEvent event ) {}
-	@EventSubscriber public void name( GuildEmojisUpdateEvent event ) {}
-	@EventSubscriber public void name( GuildEvent event ) {}
-	@EventSubscriber public void name( GuildLeaveEvent event ) {}
-	@EventSubscriber public void name( GuildTransferOwnershipEvent event ) {}
-	@EventSubscriber public void name( GuildUnavailableEvent event ) {}
-	@EventSubscriber public void name( GuildUpdateEvent event ) {}
+	@EventSubscriber public void onAllUsersReceivedEvent( AllUsersReceivedEvent event ) {}
+	@EventSubscriber public void onGuildCreateEvent( GuildCreateEvent event ) {}
+	@EventSubscriber public void onGuildEmojisUpdateEvent( GuildEmojisUpdateEvent event ) {}
+	@EventSubscriber public void onGuildEvent( GuildEvent event ) {}
+	@EventSubscriber public void onGuildLeaveEvent( GuildLeaveEvent event ) {}
+	@EventSubscriber public void onGuildTransferOwnershipEvent( GuildTransferOwnershipEvent event ) {}
+	@EventSubscriber public void onGuildUnavailableEvent( GuildUnavailableEvent event ) {}
+	@EventSubscriber public void onGuildUpdateEvent( GuildUpdateEvent event ) {}
 	
 	// Guild Category Events
-	@EventSubscriber public void name( CategoryCreateEvent event ) {}
-	@EventSubscriber public void name( CategoryDeleteEvent event ) {}
-	@EventSubscriber public void name( CategoryEvent event ) {}
-	@EventSubscriber public void name( CategoryUpdateEvent event ) {}
+	@EventSubscriber public void onCategoryCreateEvent( CategoryCreateEvent event ) {}
+	@EventSubscriber public void onCategoryDeleteEvent( CategoryDeleteEvent event ) {}
+	@EventSubscriber public void onCategoryEvent( CategoryEvent event ) {}
+	@EventSubscriber public void onCategoryUpdateEvent( CategoryUpdateEvent event ) {}
 
 	// Guild Channel Events
-	@EventSubscriber public void name( ChannelCategoryUpdateEvent event ) {}
-	@EventSubscriber public void name( ChannelCreateEvent event ) {}
-	@EventSubscriber public void name( ChannelDeleteEvent event ) {}
-	@EventSubscriber public void name( ChannelEvent event ) {}
-	@EventSubscriber public void name( ChannelUpdateEvent event ) {}
-	@EventSubscriber public void name( TypingEvent event ) {}
+	@EventSubscriber public void onChannelCategoryUpdateEvent( ChannelCategoryUpdateEvent event ) {}
+	@EventSubscriber public void onChannelCreateEvent( ChannelCreateEvent event ) {}
+	@EventSubscriber public void onChannelDeleteEvent( ChannelDeleteEvent event ) {}
+	@EventSubscriber public void onChannelEvent( ChannelEvent event ) {}
+	@EventSubscriber public void onChannelUpdateEvent( ChannelUpdateEvent event ) {}
+	@EventSubscriber public void onTypingEvent( TypingEvent event ) {}
 
 	//Guild Channel Message Events
-	@EventSubscriber public void name( MentionEvent event ) {}
-	@EventSubscriber public void name( MessageDeleteEvent event ) {}
-	@EventSubscriber public void name( MessageEditEvent event ) {}
-	@EventSubscriber public void name( MessageEmbedEvent event ) {}
-	@EventSubscriber public void name( MessageEvent event ) {}
-	@EventSubscriber public void name( MessagePinEvent event ) {}
-	@EventSubscriber public void name( MessageReceivedEvent event ) {}
-	@EventSubscriber public void name( MessageSendEvent event ) {}
-	@EventSubscriber public void name( MessageUnpinEvent event ) {}
-	@EventSubscriber public void name( MessageUpdateEvent event ) {}
+	@EventSubscriber public void onMentionEvent( MentionEvent event ) {}
+	@EventSubscriber public void onMessageDeleteEvent( MessageDeleteEvent event ) {}
+	@EventSubscriber public void onMessageEditEvent( MessageEditEvent event ) {}
+	@EventSubscriber public void onMessageEmbedEvent( MessageEmbedEvent event ) {}
+	@EventSubscriber public void onMessageEvent( MessageEvent event ) {}
+	@EventSubscriber public void onMessagePinEvent( MessagePinEvent event ) {}
+	@EventSubscriber public void onMessageReceivedEvent( MessageReceivedEvent event ) {}
+	@EventSubscriber public void onMessageSendEvent( MessageSendEvent event ) {}
+	@EventSubscriber public void onMessageUnpinEvent( MessageUnpinEvent event ) {}
+	@EventSubscriber public void onMessageUpdateEvent( MessageUpdateEvent event ) {}
 
 	// Guild Channel Reaction Events
-	@EventSubscriber public void name( ReactionAddEvent event ) {}
-	@EventSubscriber public void name( ReactionEvent event ) {}
-	@EventSubscriber public void name( ReactionRemoveEvent event ) {}
+	@EventSubscriber public void onReactionAddEvent( ReactionAddEvent event ) {}
+	@EventSubscriber public void onReactionEvent( ReactionEvent event ) {}
+	@EventSubscriber public void onReactionRemoveEvent( ReactionRemoveEvent event ) {}
 
 	// Guild Channel Webhook Events
-	@EventSubscriber public void name( WebhookCreateEvent event ) {}
-	@EventSubscriber public void name( WebhookDeleteEvent event ) {}
-	@EventSubscriber public void name( WebhookEvent event ) {}
-	@EventSubscriber public void name( WebhookUpdateEvent event ) {}
+	@EventSubscriber public void onWebhookCreateEvent( WebhookCreateEvent event ) {}
+	@EventSubscriber public void onWebhookDeleteEvent( WebhookDeleteEvent event ) {}
+	@EventSubscriber public void onWebhookEvent( WebhookEvent event ) {}
+	@EventSubscriber public void onWebhookUpdateEvent( WebhookUpdateEvent event ) {}
 
 	// Guild Member Events
-	@EventSubscriber public void name( GuildMemberEvent event ) {}
-	@EventSubscriber public void name( NicknameChangedEvent event ) {}
-	@EventSubscriber public void name( UserBanEvent event ) {}
-	@EventSubscriber public void name( UserJoinEvent event ) {}
-	@EventSubscriber public void name( UserLeaveEvent event ) {}
-	@EventSubscriber public void name( UserPardonEvent event ) {}
-	@EventSubscriber public void name( UserRoleUpdateEvent event ) {}
+	@EventSubscriber public void onGuildMemberEvent( GuildMemberEvent event ) {}
+	@EventSubscriber public void onNicknameChangedEvent( NicknameChangedEvent event ) {}
+	@EventSubscriber public void onUserBanEvent( UserBanEvent event ) {}
+	@EventSubscriber public void onUserJoinEvent( UserJoinEvent event ) {}
+	@EventSubscriber public void onUserLeaveEvent( UserLeaveEvent event ) {}
+	@EventSubscriber public void onUserPardonEvent( UserPardonEvent event ) {}
+	@EventSubscriber public void onUserRoleUpdateEvent( UserRoleUpdateEvent event ) {}
 
 	// Guild Role Events
-	@EventSubscriber public void name( RoleCreateEvent event ) {}
-	@EventSubscriber public void name( RoleDeleteEvent event ) {}
-	@EventSubscriber public void name( RoleEvent event ) {}
-	@EventSubscriber public void name( RoleUpdateEvent event ) {}
+	@EventSubscriber public void onRoleCreateEvent( RoleCreateEvent event ) {}
+	@EventSubscriber public void onRoleDeleteEvent( RoleDeleteEvent event ) {}
+	@EventSubscriber public void onRoleEvent( RoleEvent event ) {}
+	@EventSubscriber public void onRoleUpdateEvent( RoleUpdateEvent event ) {}
 
 	// Guild Voice Events
-	@EventSubscriber public void name( VoiceChannelCreateEvent event ) {}
-	@EventSubscriber public void name( VoiceChannelDeleteEvent event ) {}
-	@EventSubscriber public void name( VoiceChannelEvent event ) {}
-	@EventSubscriber public void name( VoiceChannelUpdateEvent event ) {}
-	@EventSubscriber public void name( VoiceDisconnectedEvent event ) {}
-	@EventSubscriber public void name( VoicePingEvent event ) {}
+	@EventSubscriber public void onVoiceChannelCreateEvent( VoiceChannelCreateEvent event ) {}
+	@EventSubscriber public void onVoiceChannelDeleteEvent( VoiceChannelDeleteEvent event ) {}
+	@EventSubscriber public void onVoiceChannelEvent( VoiceChannelEvent event ) {}
+	@EventSubscriber public void onVoiceChannelUpdateEvent( VoiceChannelUpdateEvent event ) {}
+	@EventSubscriber public void onVoiceDisconnectedEvent( VoiceDisconnectedEvent event ) {}
+	@EventSubscriber public void onVoicePingEvent( VoicePingEvent event ) {}
 
 	// Guild Voice User Events
-	@EventSubscriber public void name( UserSpeakingEvent event ) {}
-	@EventSubscriber public void name( UserVoiceChannelEvent event ) {}
-	@EventSubscriber public void name( UserVoiceChannelJoinEvent event ) {}
-	@EventSubscriber public void name( UserVoiceChannelLeaveEvent event ) {}
-	@EventSubscriber public void name( UserVoiceChannelMoveEvent event ) {}
+	@EventSubscriber public void onUserSpeakingEvent( UserSpeakingEvent event ) {}
+	@EventSubscriber public void onUserVoiceChannelEvent( UserVoiceChannelEvent event ) {}
+	@EventSubscriber public void onUserVoiceChannelJoinEvent( UserVoiceChannelJoinEvent event ) {}
+	@EventSubscriber public void onUserVoiceChannelLeaveEvent( UserVoiceChannelLeaveEvent event ) {}
+	@EventSubscriber public void onUserVoiceChannelMoveEvent( UserVoiceChannelMoveEvent event ) {}
 
 	// Module Events
-	@EventSubscriber public void name( ModuleDisabledEvent event ) {}
-	@EventSubscriber public void name( ModuleEnabledEvent event ) {}
-	@EventSubscriber public void name( ModuleEvent event ) {}
+	@EventSubscriber public void onModuleDisabledEvent( ModuleDisabledEvent event ) {}
+	@EventSubscriber public void onModuleEnabledEvent( ModuleEnabledEvent event ) {}
+	@EventSubscriber public void onModuleEvent( ModuleEvent event ) {}
 	
 	// Shard Events
-	@EventSubscriber public void name( DisconnectedEvent event ) {}
-	@EventSubscriber public void name( LoginEvent event ) {}
-	@EventSubscriber public void name( ReconnectFailureEvent event ) {}
-	@EventSubscriber public void name( ReconnectSuccessEvent event ) {}
-	@EventSubscriber public void name( ResumedEvent event ) {}
-	@EventSubscriber public void name( ShardEvent event ) {}
-	@EventSubscriber public void name( ShardReadyEvent event ) {}
+	@EventSubscriber public void onDisconnectedEvent( DisconnectedEvent event ) {}
+	@EventSubscriber public void onLoginEvent( LoginEvent event ) {}
+	@EventSubscriber public void onReconnectFailureEvent( ReconnectFailureEvent event ) {}
+	@EventSubscriber public void onReconnectSuccessEvent( ReconnectSuccessEvent event ) {}
+	@EventSubscriber public void onResumedEvent( ResumedEvent event ) {}
+	@EventSubscriber public void onShardEvent( ShardEvent event ) {}
+	@EventSubscriber public void onShardReadyEvent( ShardReadyEvent event ) {}
 	
 	// User Events
-	@EventSubscriber public void name( PresenceUpdateEvent event ) {}
-	@EventSubscriber public void name( UserEvent event ) {}
-	@EventSubscriber public void name( UserUpdateEvent event ) {}
+	@EventSubscriber public void onPresenceUpdateEvent( PresenceUpdateEvent event ) {}
+	@EventSubscriber public void onUserEvent( UserEvent event ) {}
+	@EventSubscriber public void onUserUpdateEvent( UserUpdateEvent event ) {}
 	
 	// Audio Events
-	@EventSubscriber public void name( AudioPlayerCleanEvent event ) {}
-	@EventSubscriber public void name( AudioPlayerEvent event ) {}
-	@EventSubscriber public void name( AudioPlayerInitEvent event ) {}
-	@EventSubscriber public void name( LoopStateChangeEvent event ) {}
-	@EventSubscriber public void name( PauseStateChangeEvent event ) {}
-	@EventSubscriber public void name( ProcessorAddEvent event ) {}
-	@EventSubscriber public void name( ProcessorRemoveEvent event ) {}
-	@EventSubscriber public void name( ShuffleEvent event ) {}
-	@EventSubscriber public void name( TrackFinishEvent event ) {}
-	@EventSubscriber public void name( TrackQueueEvent event ) {}
-	@EventSubscriber public void name( TrackSkipEvent event ) {}
-	@EventSubscriber public void name( TrackStartEvent event ) {}
-	@EventSubscriber public void name( VolumeChangeEvent event ) {}
+	@EventSubscriber public void onAudioPlayerCleanEvent( AudioPlayerCleanEvent event ) {}
+	@EventSubscriber public void onAudioPlayerEvent( AudioPlayerEvent event ) {}
+	@EventSubscriber public void onAudioPlayerInitEvent( AudioPlayerInitEvent event ) {}
+	@EventSubscriber public void onLoopStateChangeEvent( LoopStateChangeEvent event ) {}
+	@EventSubscriber public void onPauseStateChangeEvent( PauseStateChangeEvent event ) {}
+	@EventSubscriber public void onProcessorAddEvent( ProcessorAddEvent event ) {}
+	@EventSubscriber public void onProcessorRemoveEvent( ProcessorRemoveEvent event ) {}
+	@EventSubscriber public void onShuffleEvent( ShuffleEvent event ) {}
+	@EventSubscriber public void onTrackFinishEvent( TrackFinishEvent event ) {}
+	@EventSubscriber public void onTrackQueueEvent( TrackQueueEvent event ) {}
+	@EventSubscriber public void onTrackSkipEvent( TrackSkipEvent event ) {}
+	@EventSubscriber public void onTrackStartEvent( TrackStartEvent event ) {}
+	@EventSubscriber public void onVolumeChangeEvent( VolumeChangeEvent event ) {}
 
 }
