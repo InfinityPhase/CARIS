684bec2974a8dabae7c12297f6d1664884d3049d
InfinityPhase <4tazracer@gmail.com>
2018-01-03 19:05:45 -0800

    Added ability to get and set blacklists and whitelists

--- src/library/Variables.java
+++ src/library/Variables.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -30,7 +31,7 @@ public class Variables {
 	 * The database is accessed using the Database class, which provides a wrapper to
 	 * the currently used database software, SQLite.
 	 */
-	
+
 	private static Database server = null;
 	private static SimpleDateFormat format = new SimpleDateFormat("YYYYMMddhhmmss", Locale.ENGLISH);
 
@@ -52,30 +53,31 @@ public class Variables {
 	public static void init( String file ) {
 
 		server = new Database( file );
-		
+
 		/* Set the nessessary PRAGMA */
-		
+
 		server.setPragma( "foreign_keys", true ); // Make sure tables that are dependent on others work right
-		
+
 		/* Create tables, collumns, if nessessary */
-		
+
 		server.makeTable( "Guild", new ArrayList<String>() {{
 			add("guild_id integer PRIMARY KEY NOT NULL"); // The primary id, what it says on the tin.
-			
+
 			// Keys to the kingdom
-			add("modules_id integer NOT NULL"); 
-			add("polls_id integer NOT NULL"); 
-			add("locations_id integer NOT NULL"); // Are locations even used?
-			add("people_id integer NOT NULL");
-			add("translator_id integer NOT NULL"); 
-			add("userIndex_id integer NOT NULL"); // Make this table set of stuff and pain
-			add("reminders_id integer NOT NULL"); 
-			add("blacklist_id integer NOT NULL"); 
-			add("whitelist_id integer NOT NULL");
-			
+			// I wonder if all of these could be replaced with one collumn, that could be linked to each required table...
+			add("modules_id integer NOT NULL AUTOINCREMENT"); 
+			add("polls_id integer NOT NULL AUTOINCREMENT"); 
+			add("locations_id integer NOT NULL AUTOINCREMENT"); // Are locations even used?
+			add("people_id integer NOT NULL AUTOINCREMENT");
+			add("translator_id integer NOT NULL AUTOINCREMENT"); 
+			add("userIndex_id integer NOT NULL AUTOINCREMENT"); // Make this table set of stuff and pain
+			add("reminders_id integer NOT NULL AUTOINCREMENT"); 
+			add("blacklist_id integer NOT NULL AUTOINCREMENT"); 
+			add("whitelist_id integer NOT NULL AUTOINCREMENT");
+
 			// Don't need to refrence other tables
 			add("pollBuilder text NOT NULL"); add("moduleStatusBuilder text NOT NULL"); add("logChannel Integer"); // Some of these may actually be null. I dunno.
-			
+
 			// It seems all these lines must be placed last, for whatever reason. Argh.
 			add("FOREIGN KEY (modules_id) REFERENCES Modules(modules_id)"); add("FOREIGN KEY (polls_id) REFERENCES Polls(polls_id)"); 
 			add("FOREIGN KEY (locations_id) REFERENCES Locations(locations_id)"); add("FOREIGN KEY (people_id) REFERENCES People(people_id)");
@@ -83,60 +85,60 @@ public class Variables {
 			add("FOREIGN KEY (reminders_id) REFERENCES Reminders(reminders_id)"); add("FOREIGN KEY (blacklist_id) REFERENCES Blacklist(blacklist_id)"); 
 			add("FOREIGN KEY (whitelist_id) REFERENCES Whitelist(whitelist_id)");
 		}});
-		
+
 		server.makeTable( "Modules", new ArrayList<String>() {{
 			add("modules_id integer PRIMARY KEY NOT NULL");
 			add("name text NOT NULL"); add("status integer NOT NULL"); // See https://stackoverflow.com/a/843786 for why status is an integer, not a boolean
 		}});
-		
+
 		server.makeTable( "Polls", new ArrayList<String>() {{
 			add("polls_id integer PRIMARY KEY NOT NULL"); 
 			add("name text NOT NULL"); add("poll text NOT NULL");
 		}});
-		
+
 		server.makeTable( "Locations", new ArrayList<String>() {{
 			add("locations_id integer PRIMARY KEY NOT NULL"); 
 			add("name text NOT NULL");
 			add("place_id integer NOT NULL"); add("FOREIGN KEY (place_id) REFERENCES Location_P2(place_id)"); 
 		}});
-		
+
 		server.makeTable( "Location_P2", new ArrayList<String>() {{ // Rename to Place?
 			add("place_id integer PRIMARY KEY NOT NULL"); 
 			add("name text NOT NULL");
 		}});
-		
+
 		server.makeTable( "People", new ArrayList<String>() {{
 			add("people_id integer PRIMARY KEY NOT NULL");
 			add("person text NOT NULL"); add("place text NOT NULL");
 		}});
-		
+
 		server.makeTable( "Translator", new ArrayList<String>() {{
 			add("translator_id integer PRIMARY KEY NOT NULL"); 
 			add("name text NOT NULL"); add("otherName text NOT NULL");
 		}});
-		
+
 		server.makeTable( "UserIndex", new ArrayList<String>() {{
 			add("userIndex_id integer PRIMARY KEY NOT NULL"); 
 			add("userID integer NOT NULL"); 
 			add("userInfo_id integer NOT NULL"); add("FOREIGN KEY (userInfo_id) REFERENCES UserInfo(userInfo_id)"); // Maybe this can be temporarily null? 
 		}});
-		
+
 		server.makeTable( "Reminders", new ArrayList<String>() {{
 			add("reminders_id integer PRIMARY KEY NOT NULL"); 
 			add("time text NOT NULL"); 
 			add("reminderData_id integer NOT NULL"); add("FOREIGN KEY (reminderData_id) REFERENCES ReminderData(reminderData_id)"); 
 		}});
-		
+
 		server.makeTable( "ReminderData", new ArrayList<String>() {{
 			add("reminderData_id integer PRIMARY KEY NOT NULL"); 
 			add("message text NOT NULL"); add("author text NOT NULL"); add("channelID text NOT NULL"); 
 		}});
-		
+
 		server.makeTable( "Blacklist", new ArrayList<String>() {{
 			add("blacklist_id integer PRIMARY KEY NOT NULL");
 			add("channelID integer NOT NULL"); 
 		}});
-		
+
 		server.makeTable( "Whitelist", new ArrayList<String>() {{
 			add("whitelist_id integer PRIMARY KEY NOT NULL"); 
 			add("channelID integer NOT NULL"); 
@@ -144,7 +146,7 @@ public class Variables {
 	}
 
 	/* Functions to get important shit from the database */
-	
+
 	/* NEED TO BE ABLE TO:
 	 * modules and status
 	 * Polls
@@ -155,11 +157,12 @@ public class Variables {
 	 * Reminders
 	 * Blacklist
 	 * Whitelist
-	 * PollBuilder
-	 * moduleStatusBuilder
-	 * logChannel
+	 * PollBuilder May not need
+	 * moduleStatusBuilder May not need
+	 * logChannel GS Done
 	 */
 
+	// These are old, check validity
 	public static IChannel getChannel( String key ) {
 		return getChannel( new Long( key ) );
 	}
@@ -167,11 +170,11 @@ public class Variables {
 	public static IChannel getChannel( long key ) {
 		return Brain.cli.getChannelByID( key );
 	}
-	
+
 	public static Map<String, Boolean> getGuildModules( String guildID ) {
 		return getGuildModules( new Long(guildID ) );
 	}
-	
+
 	public static Map<String, Boolean> getGuildModules( long guildID ) {
 		return getGuildModules( null ); // TODO FIX ME
 	}
@@ -210,16 +213,64 @@ public class Variables {
 		}
 
 		return result;
+	} // End old functions
+	
+	public List<Long> getWhitelist( IGuild guild ) {
+		return getWhitelist( guild.getStringID() );
 	}
 	
+	public List<Long> getWhitelist( long guild ){
+		return getWhitelist( String.valueOf(guild) );
+	}
+	
+	public List<Long> getWhitelist( String guild ){
+		ArrayList<Long> result = new ArrayList<Long>();
+		ResultSet rs = server.query( "SELECT whitelist_id FROM guild WHERE guild_id = " + guild );
+		
+		try {
+			ResultSet rs2 = server.query( "SELECT channelID FROM Whitelist WHERE whitelist_id = " + rs.getString( "whitelist_id" ) + ";" );
+			while( rs2.next() ) {
+				result.add( rs2.getLong( "channelID" ) );
+			}
+		} catch( SQLException e ) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public List<Long> getBlacklist( IGuild guild ) {
+		return getBlacklist( guild.getStringID() );
+	}
+	
+	public List<Long> getBlacklist( long guild ){
+		return getBlacklist( String.valueOf(guild) );
+	}
+	
+	public List<Long> getBlacklist( String guild ){
+		ArrayList<Long> result = new ArrayList<Long>();
+		ResultSet rs = server.query( "SELECT blacklist_id FROM guild WHERE guild_id = " + guild );
+		
+		try {
+			ResultSet rs2 = server.query( "SELECT channelID FROM Whitelist WHERE blacklist_id = " + rs.getString( "blacklist_id" ) + ";" );
+			while( rs2.next() ) {
+				result.add( rs2.getLong( "channelID" ) );
+			}
+		} catch( SQLException e ) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+
 	public long getLogChannel( IGuild guild ) {
 		return getLogChannel( guild.getLongID() );
 	}
-	
+
 	public long getLogChannel( String guild ) {
 		return getLogChannel( new Long( guild ) );
 	}
-	
+
 	public long getLogChannel( long guild ) {
 		ResultSet rs = server.query( "SELECT logChannel FROM Guild WHERE guild_id = " + guild );
 		try {
@@ -229,39 +280,83 @@ public class Variables {
 		}
 		return -1; // I don't want to use an object, so have a negative
 	}
-	
+
 	/* Functions to set values in the database */
-	
+
+	public void addBlacklist( IGuild guild, IChannel channel ) {
+		addBlacklist( guild.getLongID(), channel.getLongID() );
+	}
+
+	public void addBlacklist( long guild, long channel ) {
+		addBlacklist( String.valueOf(guild), String.valueOf(channel) ); 
+	}
+
+	@SuppressWarnings("serial")
+	public void addBlacklist( String guild, String channel ) {
+		ResultSet rs = server.query( "SELECT blacklist_id FROM Guild WHERE guild_id = " + guild + ";" ); // Could make this a one-liner, just for kicks...
+
+		try {
+			server.insert( guild, new ArrayList<String>() {{
+				add(rs.getString("blacklist_id")); add(channel);
+			}});
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void addWhitelist( IGuild guild, IChannel channel ) {
+		addWhitelist( guild.getLongID(), channel.getLongID() );
+	}
+
+	public void addWhitelist( long guild, long channel ) {
+		addWhitelist( String.valueOf(guild), String.valueOf(channel) );
+	}
+
+	@SuppressWarnings("serial")
+	public void addWhitelist( String guild, String channel ) {
+		ResultSet rs = server.query( "SELECT whitelist_id FROM Guild WHERE guild_id = " + guild + ";" ); // Could make this a one-liner, just for kicks...
+
+		try {
+			server.insert( guild, new ArrayList<String>() {{
+				add(rs.getString("whitelist_id")); add(channel);
+			}});
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	public void setLogChannel( IGuild guild, IChannel channel ) {
 		setLogChannel( guild.getLongID(), channel.getLongID() );
 	}
-	
-	public void setLogChannel( String guild, String channel ) {
-		setLogChannel( new Long( guild ), new Long( channel ) );
-	}
-	
+
 	@SuppressWarnings("serial")
-	public void setLogChannel( long guild, long channel ) {
-		server.update( String.valueOf(guild), new HashMap<String, String>() {{
-			put("logChannel",String.valueOf(channel));
+	public void setLogChannel( String guild, String channel ) {		
+		server.update( guild, new HashMap<String, String>() {{
+			put("logChannel",channel);
 		}});
 	}
 
+	public void setLogChannel( long guild, long channel ) {
+		setLogChannel( String.valueOf(guild), String.valueOf(channel) );
+	}
+
 	/* Functions to interpret the shit returned from the database */
-	
+
 	public static String timeString( Calendar input ) {
 		return format.format( input.getTime() );
 	}
-	
+
 	public static Calendar toCalendar( String wtf ) {
 		Calendar result = Calendar.getInstance();
-		
+
 		try {
 			result.setTime( format.parse( wtf ) );
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
 
