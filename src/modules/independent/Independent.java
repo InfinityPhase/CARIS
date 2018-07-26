7a2800e1408cdbc7ad05b9750cd2f34a7aaf944a
InfinityPhase <4a.zanella@gmail.com>
2018-07-25 17:48:34 -0700

    New format for process and run functions to avoid boring code

--- src/modules/independent/Independent.java
+++ src/modules/independent/Independent.java
@@ -8,17 +8,36 @@ import tokens.Response;
 
 public class Independent extends Module {
 	List<Condition> conditions;
-	
+
 	public Independent() {
 		conditions = new ArrayList<>();
 	}
-	
+
 	public interface Condition {	
 		public boolean check();
+		public void reset(); // For repeating conditions
 	}
-	
+
 	public Response process() {
-		setup();
+		if( checkConditions() ) {
+			setup();
+			run();
+		}
+		
 		return build();
+	}
+
+	protected void run() {
+		// Skips the check
+	}
+
+	protected boolean checkConditions() {
+		for( Condition c : conditions ) {
+			if( !c.check() ) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
