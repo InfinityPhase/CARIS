e3042e5ef09705a854f8a5619e23a93b6e0bc224
Auxiliatrix <Squeakadoodle@gmail.com>
2018-08-15 01:37:22 -0700

    Added more functions to string utilities

--- src/caris/framework/utilities/StringUtilities.java
+++ src/caris/framework/utilities/StringUtilities.java
@@ -1,6 +1,7 @@
 package caris.framework.utilities;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 public class StringUtilities {
@@ -9,25 +10,11 @@ public class StringUtilities {
 		return a.toLowerCase().equals(b);
 	}
 	
-	public static Boolean containsIgnoreCase(Set<String> a, String b) {
-		for( String token : a ) {
-			if( containsIgnoreCase(token, b) ) {
-				return true;
-			}
-		}
-		return false;
+	public static boolean hasIgnoreCase(String[] a, String b) {
+		return hasIgnoreCase(Arrays.asList(a), b);
 	}
 	
-	public static boolean hasIgnoreCase(ArrayList<String> a, String b) {
-		for( String token: a ) {
-			if( token.equalsIgnoreCase(b) ) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public static boolean hasIgnoreCase(String[] a, String b) {
+	public static boolean hasIgnoreCase(List<String> a, String b) {
 		for( String token: a ) {
 			if( token.equalsIgnoreCase(b) ) {
 				return true;
@@ -49,16 +36,11 @@ public class StringUtilities {
 		return a.toLowerCase().contains(b.toLowerCase());
 	}
 
-	public static boolean containsIgnoreCase(ArrayList<String> a, String b) {
-		for( String token : a ) {
-			if( containsIgnoreCase(token, b) ) {
-				return true;
-			}
-		}
-		return false;
-	}
-
 	public static boolean containsIgnoreCase(String[] a, String b) {
+		return containsIgnoreCase(Arrays.asList(a), b);
+	}
+	
+	public static boolean containsIgnoreCase(List<String> a, String b) {
 		for( String token : a ) {
 			if( containsIgnoreCase(token, b) ) {
 				return true;
@@ -67,4 +49,38 @@ public class StringUtilities {
 		return false;
 	}
 	
+	public static Boolean containsIgnoreCase(Set<String> a, String b) {
+		for( String token : a ) {
+			if( containsIgnoreCase(token, b) ) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean containsAnyOfIgnoreCase(String a, String[] b) {
+		return containsAnyOfIgnoreCase(a, Arrays.asList(b));
+	}
+	
+	public static boolean containsAnyOfIgnoreCase(String a, List<String> b) {
+		for( String token : b ) {
+			if( containsIgnoreCase(a, token) ) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean containsAllOfIgnoreCase(String a, String[] b) {
+		return containsAllOfIgnoreCase(a, Arrays.asList(b));
+	}
+
+	public static boolean containsAllOfIgnoreCase(String a, List<String> b) {
+		for( String token : b ) {
+			if( !containsIgnoreCase(a, token) ) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
