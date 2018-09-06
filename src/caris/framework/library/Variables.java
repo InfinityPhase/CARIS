903975831bf3ff7248f72f19ec7ce58d3ea46e8d
Auxiliatrix <Squeakadoodle@gmail.com>
2018-09-06 05:24:49 -0700

    Removed datasaver

--- src/caris/framework/library/Variables.java
+++ src/caris/framework/library/Variables.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import caris.framework.utilities.DataSaver;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IUser;
 
@@ -15,7 +14,6 @@ public class Variables {
 	public static HashMap<IGuild, GuildInfo> guildIndex = new HashMap<IGuild, GuildInfo>();
 	
 	/* Global Utilities */
-	public static DataSaver ds = new DataSaver();
 	public static List<String> commandPrefixes = new ArrayList<String>();
 	public static List<String> commandExacts = new ArrayList<String>();
 	public static List<String> toolPrefixes = new ArrayList<String>();
