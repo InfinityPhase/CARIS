6cc0bb2378d91f5e9baf70c00ac590bac24b1f84
Auxiliatrix <Squeakadoodle@gmail.com>
2018-10-10 12:48:45 -0700

    A possible fix for hanging threads

--- src/caris/framework/events/EventManager.java
+++ src/caris/framework/events/EventManager.java
@@ -7,35 +7,47 @@ import caris.framework.basehandlers.Handler;
 import caris.framework.basereactions.MultiReaction;
 import caris.framework.basereactions.Reaction;
 import caris.framework.main.Brain;
+import caris.framework.utilities.Logger;
 import sx.blah.discord.api.events.Event;
 import sx.blah.discord.api.events.EventSubscriber;
 
 public class EventManager extends SuperEvent {
 
+	public int instanceCount = 0;
+	
 	@EventSubscriber
 	@Override
 	public void onEvent(Event event) {
-		ArrayList<Reaction> reactions = new ArrayList<Reaction>();
-		ArrayList<Reaction> passiveQueue = new ArrayList<Reaction>();
-		for( Handler h : Brain.handlers.values() ) {
-			Reaction r = h.handle(event);
-			if( r != null ) {
-				if( r.priority == -1 ) {
-					passiveQueue.add(r);
-				} else {
-					reactions.add(r);
+		try {
+			instanceCount++;
+			if( instanceCount > 1 ) {
+				Logger.error("Number of instances: " + instanceCount);
+			}
+			ArrayList<Reaction> reactions = new ArrayList<Reaction>();
+			ArrayList<Reaction> passiveQueue = new ArrayList<Reaction>();
+			for( Handler h : Brain.handlers.values() ) {
+				Reaction r = h.handle(event);
+				if( r != null ) {
+					if( r.priority == -1 ) {
+						passiveQueue.add(r);
+					} else {
+						reactions.add(r);
+					}
 				}
 			}
-		}
-		if( !reactions.isEmpty() ) {
-			Reaction[] options = new Reaction[reactions.size()];
-			for( int f=0; f<reactions.size(); f++ ) {
-				options[f] = reactions.get(f);
+			if( !reactions.isEmpty() ) {
+				Reaction[] options = new Reaction[reactions.size()];
+				for( int f=0; f<reactions.size(); f++ ) {
+					options[f] = reactions.get(f);
+				}
+				Arrays.sort(options);
+				options[0].run();
 			}
-			Arrays.sort(options);
-			options[0].run();
+			MultiReaction passiveQueueExecutor = new MultiReaction(passiveQueue);
+			passiveQueueExecutor.run();
+			instanceCount--;
+		} catch (Exception E) {
+			return;
 		}
-		MultiReaction passiveQueueExecutor = new MultiReaction(passiveQueue);
-		passiveQueueExecutor.run();
 	}
 }
