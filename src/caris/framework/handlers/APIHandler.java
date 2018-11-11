feba0c2d7c54d6d633808a310a2b5b5b2cccd502
Auxiliatrix <Squeakadoodle@gmail.com>
2018-11-11 04:15:29 -0800

    I'm not sure waht I fixed but it works now

--- src/caris/framework/handlers/APIHandler.java
+++ src/caris/framework/handlers/APIHandler.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import caris.framework.basehandlers.MessageHandler;
 import caris.framework.basereactions.Reaction;
 import caris.framework.reactions.ReactionMessage;
+import caris.framework.utilities.Logger;
 import caris.framework.utilities.TheBlueAllianceAPI;
 import caris.framework.utilities.TokenUtilities;
 import sx.blah.discord.api.events.Event;
@@ -14,7 +15,7 @@ public class APIHandler extends MessageHandler {
 	public APIHandler() {
 		super("API", Access.DEFAULT, false);
 		description = "Retrieves the raw JSON body from an API endpoint.";
-		usage.put(getKeyword() + "\"path\"", "Prints the results of the specified path from the TBA endpoint.");
+		usage.put(getKeyword() + " \"path\"", "Prints the results of the specified path from the TBA endpoint.");
 	}
 	
 	@Override
@@ -24,11 +25,15 @@ public class APIHandler extends MessageHandler {
 	
 	@Override
 	public Reaction process(Event event) {
+		Logger.debug("API detected", 2);
 		ArrayList<String> quoted = TokenUtilities.parseQuoted(message);
 		if( quoted.size() > 0 ) {
-			return new ReactionMessage("" + TheBlueAllianceAPI.getJSON(quoted.get(0)), mrEvent.getChannel());
+			Logger.debug("Response produced from " + name, 1, true);
+			return new ReactionMessage("" + TheBlueAllianceAPI.getJSON(quoted.get(0)), mrEvent.getChannel(), 1);
 		} else {
-			return new ReactionMessage("You need to specify the path! Did you put them in quotes?", mrEvent.getChannel());
+			Logger.debug("Operation failed because no path specified", 2);
+			Logger.debug("Response produced from " + name, 1, true);
+			return new ReactionMessage("You need to specify the path! Did you put them in quotes?", mrEvent.getChannel(), 1);
 		}
 	}
 	
