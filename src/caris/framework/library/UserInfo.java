afa18356545c050d8a1975a4157b0d7080201c3f
Auxiliatrix <Squeakadoodle@gmail.com>
2018-12-04 17:17:07 -0800

    Removed presence update and mail because too much lag

--- src/caris/framework/library/UserInfo.java
+++ src/caris/framework/library/UserInfo.java
@@ -1,9 +1,7 @@
 package caris.framework.library;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
-import caris.framework.tokens.Mail;
 import sx.blah.discord.handle.obj.IUser;
 
 public class UserInfo {
@@ -13,9 +11,7 @@ public class UserInfo {
 	public String lastMessage;
 	
 	public String nicknameLock;
-	
-	public Mailbox mailbox;
-	
+		
 	public HashMap< String, Object > genericInfo;
 	
 	public UserInfo( IUser user ) {
@@ -23,51 +19,7 @@ public class UserInfo {
 		location = "";
 		
 		nicknameLock = "";
-		
-		mailbox = new Mailbox();
-		
+				
 		genericInfo = new HashMap<String, Object>();
-	}
-	
-	public class Mailbox {
-		
-		private ArrayList<Mail> inbox;
-		
-		public Mailbox() {
-			this.inbox = new ArrayList<Mail>();
-		}
-		
-		public boolean isEmpty() {
-			for( Mail mail : inbox ) {
-				if( !mail.read ) {
-					return false;
-				}
-			}
-			return true;
- 		}
-		
-		public ArrayList<Mail> getMail() {
-			return inbox;
-		}
-		
-		public Mail get(int number) {
-			return inbox.get(number);
-		}
-		
-		public Mail remove(int number) {
-			return inbox.remove(number);
-		}
-		
-		public int size() {
-			return inbox.size();
-		}
-		
-		public void clear() {
-			inbox.clear();
-		}
-
-		public void add(Mail mail) {
-			inbox.add(0, mail);
-		}
-	}
+	}	
 }
